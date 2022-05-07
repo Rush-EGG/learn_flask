b6a34ebb82be8bfb211140deb4195ef4826cc048
Rush_egg <huzhoutt@163.com>
2022-05-07 22:43:05 +0800

    first commit

--- s1.py
+++ s1.py
@@ -1,11 +1,18 @@
 from flask import Flask
 
+# 创建flask函数
 app = Flask(__name__)
 
 
 @app.route('/index')
 def index():
     return 'Hello World'
+
+
+@app.route('/login')
+def index():
+    return '登录页面'
+
 
 if __name__ == '__main__':
     app.run()
